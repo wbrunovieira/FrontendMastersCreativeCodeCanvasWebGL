6a519b38a294cc23d068af7c6e39a5b6af835f82
wbrunovieira <wbrunovieira77@gmail.com>
2024-02-26 19:24:17 -0300

    made simples shapes with canvas

--- sketch.js
+++ sketch.js
@@ -8,6 +8,14 @@ const sketch = () => {
   return ({ context, width, height }) => {
     context.fillStyle = 'orange';
     context.fillRect(0, 0, width, height);
+
+    context.beginPath();
+    context.arc(width / 2, height / 2, 200, 0, Math.PI * 2, false);
+    context.fillStyle = 'red';
+    context.fill();
+    context.lineWidth = 40;
+    context.strokeStyle = 'blue';
+    context.stroke();
   };
 };
 
