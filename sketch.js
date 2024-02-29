9dfb39043284dda9bcec798a6745187fe3433537
wbrunovieira <wbrunovieira77@gmail.com>
2024-02-29 17:05:04 -0300

    added random

--- sketch.js
+++ sketch.js
@@ -1,43 +1,41 @@
 const canvasSketch = require('canvas-sketch');
 const { lerp } = require('canvas-sketch-util/math');
+const random = require('canvas-sketch-util/random');
 
 const settings = {
   dimensions: [2048, 2048],
 };
 
-const sketch = () => {
+const sketch = ({ update }) => {
+  const count = 40;
+
   const createGrid = () => {
     const points = [];
-    const count = 5;
-
     for (let x = 0; x < count; x++) {
       for (let y = 0; y < count; y++) {
-        const u = count <= 1 ? 0.5 : x / (count - 1);
-        const v = count <= 1 ? 0.5 : y / (count - 1);
-
+        const u = x / (count - 1);
+        const v = y / (count - 1);
         points.push([u, v]);
       }
     }
-
     return points;
   };
-
-  const points = createGrid();
+  random.setSeed(512);
+  const points = createGrid().filter(() => random.value() > 0.5);
   const margin = 400;
 
-  return ({ context, width, height }) => {
+  return ({ context, width, height, params }) => {
     context.fillStyle = 'white';
     context.fillRect(0, 0, width, height);
 
     points.forEach(([u, v]) => {
       const x = lerp(margin, width - margin, u);
-
-      const y = lerp(margin, width - margin, v);
+      const y = lerp(margin, height - margin, v);
 
       context.beginPath();
-      context.arc(x, y, 100, 0, Math.PI * 2, false);
+      context.arc(x, y, 10, 0, Math.PI * 2);
       context.strokeStyle = 'black';
-      context.lineWidth = 40;
+      context.lineWidth = 5;
       context.stroke();
     });
   };
