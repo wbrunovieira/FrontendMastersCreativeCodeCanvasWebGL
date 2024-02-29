7958aec68ca2f08dbfb92d1d50ff8e24c3441641
wbrunovieira <wbrunovieira77@gmail.com>
2024-02-29 16:13:50 -0300

    create for loop for circles

--- sketch.js
+++ sketch.js
@@ -5,17 +5,36 @@ const settings = {
 };
 
 const sketch = () => {
+  const createGrid = () => {
+    const points = [];
+    const count = 5;
+    for (let x = 0; x < count; x++) {
+      for (let y = 0; y < count; y++) {
+        const u = count <= 1 ? 0.5 : x / (count - 1);
+        const v = count <= 1 ? 0.5 : x / (count - 1);
+        points.push([u, v]);
+      }
+    }
+    return points;
+  };
+
+  const points = createGrid();
+
   return ({ context, width, height }) => {
-    context.fillStyle = 'orange';
+    context.fillStyle = 'white';
     context.fillRect(0, 0, width, height);
 
-    context.beginPath();
-    context.arc(width / 2, height / 2, 200, 0, Math.PI * 2, false);
-    context.fillStyle = 'red';
-    context.fill();
-    context.lineWidth = 40;
-    context.strokeStyle = 'blue';
-    context.stroke();
+    points.forEach(([u, v]) => {
+      const x = u * width;
+
+      const y = v * height;
+
+      context.beginPath();
+      context.arc(x, y, 50, 0, Math.PI * 2, false);
+      context.strokeStyle = 'black';
+      context.lineWidth = 40;
+      context.stroke();
+    });
   };
 };
 
